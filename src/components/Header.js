516222b0371f7a9e356ffd1f9bbd3b10b7a66997
Robert Var <mail.robertvari@gmail.com>
2022-11-12 17:37:28 +0100

    update

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,20 @@
 import React from 'react'
 import SocialLinks from './micro/SocialLinks'
+import {Link} from "react-router-dom"
 
 export default function Header() {
   return (
     <header>
-      <a href="" className='page-title-container'>
+      <Link to="/" className='page-title-container'>
         <h1>Atum Hartwin</h1>
         <div>photography</div>
-      </a>
+      </Link>
 
 
       <nav>
-        <a href="">about</a>
-        <a href="">photos</a>
-        <a href="">contact</a>
+        <Link to="/about">about</Link>
+        <Link to="/photos">photos</Link>
+        <Link to="/contact">contact</Link>
       </nav>
 
       <SocialLinks/>
