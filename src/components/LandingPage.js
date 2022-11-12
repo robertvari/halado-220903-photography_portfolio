35b68b9064bb726b73acdead5ecfce6f39ecf2e8
Robert Var <mail.robertvari@gmail.com>
2022-11-12 17:20:27 +0100

    update

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -4,7 +4,7 @@ import ImageSlider from './micro/ImageSlider'
 export default function LandingPage() {
   return (
     <div>
-      <ImageSlider images={["https://source.unsplash.com/random/?city&1"]} title="Test title" slogen="Slogen"/>
+      <ImageSlider images={["https://source.unsplash.com/random/?city&1"]} title="Ut nec vulputate erat" slogen="Nunc sodales velit quis sem ullamcorper euismod. Morbi elementum risus non arcu pellentesque pellentesque. Duis id neque dui. Etiam ut pellentesque tellus, nec mollis odio."/>
     </div>
   )
 }
--- src/components/micro/ImageSlider.js
+++ src/components/micro/ImageSlider.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function ImageSlider({images, title, slogen}) {
   return (
-    <div className='image-slider-container' style={{backgroundImage: `url(${images[0]})`}}>
-        {/* <img src={images[0]} alt="" /> */}
+    <div className='image-slider-container'>
+        <img src={images[0]} alt="" />
         <h1>{title}</h1>
         <h2>{slogen}</h2>
     </div>
