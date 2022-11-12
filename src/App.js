5bb99447cdd15b2acff6bf498b59183be62f4073
Robert Var <mail.robertvari@gmail.com>
2022-11-12 15:12:50 +0100

    css style

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ function App() {
   return (
     <div className="App">
       <h1>Photography portfolio</h1>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Labore suscipit non veniam id, laboriosam ducimus, assumenda alias ratione vitae quos doloremque ad obcaecati asperiores quae quam. Nam sed maiores hic in repellat eligendi illum vero aliquid optio, delectus autem voluptatibus perspiciatis adipisci ducimus dolores ea, dolor quidem esse? Quo accusantium omnis quos voluptate neque facere numquam quae nulla veniam, officiis porro non eaque vel iste aspernatur explicabo nesciunt animi ullam. Culpa, veniam quod recusandae impedit veritatis vitae numquam aspernatur placeat eligendi reiciendis repellendus possimus. Architecto reprehenderit vitae reiciendis, sapiente nobis fuga expedita ad nulla dolores beatae id facere tenetur hic.</p>
     </div>
   );
 }
