35b80c8be8d605f4eae536c4014ab0a6fc479147
Robert Var <mail.robertvari@gmail.com>
2022-11-13 15:23:09 +0100

    Contact page

--- src/components/pages/ContactPage.js
+++ src/components/pages/ContactPage.js
@@ -2,6 +2,25 @@ import React from 'react'
 
 export default function ContactPage() {
   return (
-    <div>ContactPage</div>
+    <div className='contact-container'>
+      <h1>Let's work together</h1>
+      <h2>atomhartwin@gmail.com</h2>
+
+      <form action="">
+        <h2>name</h2>
+        <input type="text" />
+
+        <h2>email</h2>
+        <input type="text" />
+
+        <h2>phone</h2>
+        <input type="text" />
+
+        <h2>message</h2>
+        <textarea name="" id="" cols="30" rows="10"></textarea>
+      </form>
+
+      <button>send</button>
+    </div>
   )
 }
