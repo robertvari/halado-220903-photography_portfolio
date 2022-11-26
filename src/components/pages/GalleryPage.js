13fb6ff1f7ef601e3e0a6348c1824df4edc7bfd1
Robert Var <mail.robertvari@gmail.com>
2022-11-26 15:44:17 +0100

    Gallery page anims

--- src/components/pages/GalleryPage.js
+++ src/components/pages/GalleryPage.js
@@ -2,6 +2,8 @@ import axios from 'axios'
 import React, {useState, useEffect} from 'react'
 import {useParams} from "react-router-dom"
 import Masonry from 'react-masonry-css'
+import {motion} from "framer-motion"
+
 
 export default function GalleryPage() {
     const {category} = useParams()
@@ -26,7 +28,10 @@ export default function GalleryPage() {
     return (
         <div className='gallery-container'>
             {
-                current_image&& <div className='image-viewer' onClick={e => set_current_image(null)}><img src={current_image} alt="" /></div>
+                current_image&& 
+                <div className='image-viewer' onClick={e => set_current_image(null)}>
+                    <motion.img src={current_image} alt="" initial={{opacity:0, scale:0.8}} animate={{opacity:1, scale:1}}/>
+                </div>
             }
 
             <Masonry
@@ -35,8 +40,14 @@ export default function GalleryPage() {
                 columnClassName="my-masonry-grid_column">
                 {
                     images.filter(image_data => image_data.category === category)
-                    .map(data => 
-                    <img key={data.id} src={data.image} onClick={e => set_current_image(data.image)}/>
+                    .map((data, index) => 
+                        <motion.img 
+                        key={data.id} 
+                        src={data.image} 
+                        onClick={e => set_current_image(data.image)}
+                        initial={{opacity:0}}
+                        animate={{opacity:1, transition:{delay: index*0.1}}}
+                        />
                     )
                 }
             </Masonry>
